b7193f0d37511cd7906dc768d9285c37793fd983
Toshiki Asamoto <asamottoo.works@gmail.com>
2019-05-09 22:40:43 +0900

    リロード処理が動くよう変更した。

--- src/main/kotlin/org/pickles/submissions/presentation/controller/ClockController.kt
+++ src/main/kotlin/org/pickles/submissions/presentation/controller/ClockController.kt
@@ -3,8 +3,6 @@ package org.pickles.submissions.presentation.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 
 @Controller
 class ClockController {
@@ -12,7 +10,6 @@ class ClockController {
     @GetMapping
     fun index(model: Model): String {
         model.addAttribute("backgroundImage", getImageURL())
-        model.addAttribute("time", getTime())
         return "index"
     }
 
@@ -20,9 +17,5 @@ class ClockController {
         // 本来はここで出す画像を選択するのだけど、めんどくさいのでランダムで画像が取れるURLを固定で返す
         // TODO: いい感じにランダムで画像URLが帰るような実装にする
         return "https://picsum.photos/1920/1080"
-    }
-
-    private fun getTime(): String {
-        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
     }
 }
