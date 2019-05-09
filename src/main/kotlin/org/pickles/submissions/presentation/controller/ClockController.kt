8443d948a902405c989bb59a7b068cd00c5c0588
Toshiki Asamoto <asamottoo.works@gmail.com>
2019-05-09 22:33:00 +0900

    現在時間が表示されるよう変更した。

--- src/main/kotlin/org/pickles/submissions/presentation/controller/ClockController.kt
+++ src/main/kotlin/org/pickles/submissions/presentation/controller/ClockController.kt
@@ -3,6 +3,8 @@ package org.pickles.submissions.presentation.controller
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Controller
 class ClockController {
@@ -10,6 +12,7 @@ class ClockController {
     @GetMapping
     fun index(model: Model): String {
         model.addAttribute("backgroundImage", getImageURL())
+        model.addAttribute("time", getTime())
         return "index"
     }
 
@@ -17,5 +20,9 @@ class ClockController {
         // 本来はここで出す画像を選択するのだけど、めんどくさいのでランダムで画像が取れるURLを固定で返す
         // TODO: いい感じにランダムで画像URLが帰るような実装にする
         return "https://picsum.photos/1920/1080"
+    }
+
+    private fun getTime(): String {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm"))
     }
 }
